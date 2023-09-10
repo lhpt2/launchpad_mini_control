5fe5cadfdbf2579c4ca102395938c2c1ca4d4ad2
Lucas Haupt <lucas.haupt@t-online.de>
2023-09-10 23:40:07 +0200

    make important entities public

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
-pub mod launch_device;
-pub mod midilib;
+mod launch_device;
+pub use launch_device::*;
+mod midilib;
+pub use midilib::*;
+mod pm_impl;
+pub use pm_impl::*;
+
+pub const BUFFER_SIZE: usize = 1024;
