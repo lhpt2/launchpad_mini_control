ebb79c35a4d6a60d3a0553cf3a889636502d34ad
Lucas Haupt <lucas.haupt@t-online.de>
2023-09-17 17:07:33 +0200

    Structure of crate changed (subfolders for helper types)

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,16 @@ This program is distributed under the terms of the
 GNU Lesser General Public License v3.0, 
 see COPYING.LESSER file for license information
 */
-mod help_types;
+mod utils;
 mod launch_device;
-mod mat_pos;
 mod midilib;
-mod pad_identifier;
 mod pm_impl;
 
 pub use launch_device::*;
 pub use midilib::*;
 pub use pm_impl::*;
 
-pub use help_types::Color;
-pub use mat_pos::MatPos;
+pub use utils::Color;
+pub use utils::MatPos;
 
 pub const BUFFER_SIZE: usize = 1024;
---
+++ src/utils/mod.rs
@@ -0,0 +1,7 @@
+mod mat_pos;
+mod pad_identifier;
+mod misc;
+
+pub use self::mat_pos::*;
+pub use self::pad_identifier::*;
+pub use self::misc::*;
